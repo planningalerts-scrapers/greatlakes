243bbec924e11f9cce428b6c326334b2b054d870
Matthew Landauer <mlandauer@gmail.com>
2019-06-04 14:06:40 +1000

    Remove the first part of the address

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,10 @@
 require "epathway_scraper"
 
-EpathwayScraper.scrape_and_save(
+EpathwayScraper.scrape(
   "https://services.greatlakes.nsw.gov.au/ePathway/Production",
   list_type: :all, max_pages: 10
-)
+) do |record|
+  # Remove the first bit of the address as it just contains lot information
+  record["address"] = record["address"].split(", ")[1..-1].join(", ")
+  EpathwayScraper.save(record)
+end
