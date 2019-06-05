660bbf61c14332e026f75dee02a08a7c71439f0e
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 13:53:02 +1000

    Update to latest version of gem

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,3 @@
 require "epathway_scraper"
 
-EpathwayScraper.scrape_and_save(
-  "https://services.greatlakes.nsw.gov.au/ePathway/Production",
-  list_type: :all, max_pages: 10, state: "NSW"
-)
+EpathwayScraper.scrape_and_save_authority(:greatlakes)
