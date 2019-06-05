dda40dcb8e058fa7e380c4f414bbd26c69571f5a
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 11:10:46 +1000

    Upgrade to latest version of the gem.
    A few of the changed addresses are bogus because for some reason the 
    suburb doesn't match up with the suburb in the address and so the 
    scraper now appends it.

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,6 @@
 require "epathway_scraper"
 
-EpathwayScraper.scrape(
+EpathwayScraper.scrape_and_save(
   "https://services.greatlakes.nsw.gov.au/ePathway/Production",
   list_type: :all, max_pages: 10
-) do |record|
-  # Remove the first bit of the address as it just contains lot information
-  record["address"] = record["address"].split(", ")[1..-1].join(", ")
-  EpathwayScraper.save(record)
-end
+)
